59a12aae9d141e7baa07ad5a4489b4bb3d3c27f7
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-10-28 14:51:09 -0700

    Test approved_choice?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -12,7 +12,29 @@ describe Board do
         allow(board).to receive(:available_column?).and_return(true)
       end
       it 'returns true' do
-        expect(board.approved_choice?(4)).to be true
+        expect(board.approved_choice?('4')).to be true
+      end
+    end
+    
+    context 'when player pick is valid number and not available' do
+      before do
+        allow(board).to receive(:valid_column?).and_return(true)
+        allow(board).to receive(:available_column?).and_return(false)
+      end
+
+      it 'returns true' do
+        expect(board.approved_choice?('6')).to be false
+      end 
+    end
+
+    context 'when player pick is not a valid column' do
+      before do
+        allow(board).to receive(:valid_column?).and_return(false)
+        allow(board).to receive(:available_column?).and_return(true)
+      end
+
+      it 'returns false' do
+        expect(board.approved_choice?('a')).to be false
       end
     end
   end
