d0b5c3c3f57fb77e00a92fed844ceb6aa37be356
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-10-28 11:48:19 -0700

    Test approved_choice? when valid and avail

---
+++ spec/board_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require_relative '../lib/board'
+
+describe Board do
+  subject(:board) {described_class.new}
+
+  describe '#approved_choice?' do
+    context 'when player pick valid and available' do
+      before do
+        allow(board).to receive(:valid_column?).and_return(true)
+        allow(board).to receive(:available_column?).and_return(true)
+      end
+      it 'returns true' do
+        expect(board.approved_choice?(4)).to be true
+      end
+    end
+  end
+end
