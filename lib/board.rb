8ff330d555cea979ee9625a014b055338b6ac551
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-10-29 08:32:30 -0700

    Write method for approved_choice?

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Board
-  def valid_column?
-    
+  def valid_column?(choice)
+    valid_cols = ['1', '2', '3', '4', '5', '6', '7']
+    return true if valid_cols.include?(choice)
+    return false
   end
 
   def available_column?
@@ -10,7 +12,7 @@ class Board
   end
 
   def approved_choice?(choice)
-    return false unless valid_column?
+    return false unless valid_column?(choice)
     return false unless available_column?
     true
   end
