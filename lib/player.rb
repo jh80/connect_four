193df6dd2bdebcded256d1a173f48e52a0627a69
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-10-30 10:35:21 -0700

    Enable getting choice from player

--- lib/player.rb
+++ lib/player.rb
@@ -2,6 +2,11 @@
 
 class Player
   def initialize(name, mark)
-    
+    @name = name
+  end
+
+  def get_choice
+    puts "#{@name}, enter the number of the column you would like to place your token in."
+    gets.chomp
   end
 end
